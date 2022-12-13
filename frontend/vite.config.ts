617df8b547aa7638266d6c7f100e56d1ec04ea3d
Chance <chancesmithb@gmail.com>
2022-12-13 15:47:31 -0600

    📦  FEAT: make vite module

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -5,4 +6,24 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "./",
   plugins: [react()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "./src/main.tsx"),
+      name: "AA_NewFeatures",
+      // the proper extensions will be added
+      fileName: "aa-new-features",
+    },
+    // rollupOptions: {
+    //   // make sure to externalize deps that shouldn't be bundled
+    //   // into your library
+    //   external: ['react'],
+    //   output: {
+    //     // Provide global variables to use in the UMD build
+    //     // for externalized deps
+    //     globals: {
+    //       react: 'React',
+    //     },
+    //   },
+  },
 });
