4809ff8831d977cfd83dd6f4c71bc6c96e1ca800
Chance <chancesmithb@gmail.com>
2022-12-15 08:57:19 -0600

    🤞  TRY: start over

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,33 +1,16 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
   build: {
-    target: "modules",
     lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "./src/App.tsx"),
-      name: "AANewFeatures",
-      formats: ["umd"],
-      // the proper extensions will be added
-      fileName: "aa-new-features",
+      entry: "src/App.tsx",
+      name: "AAFeats",
+      formats: ["es"],
+      fileName: "aa-feats",
     },
-    // rollupOptions: {
-    //   // make sure to externalize deps that shouldn't be bundled
-    //   // into your library
-    //   external: ['react'],
-    //   output: {
-    //     // Provide global variables to use in the UMD build
-    //     // for externalized deps
-    //     globals: {
-    //       react: 'React',
-    //     },
-    //   },
-    // },
   },
   define: {
     process: JSON.stringify({
@@ -36,7 +19,4 @@ export default defineConfig({
       },
     }),
   },
-  appType: "custom",
-  // "process.env.NODE_ENV": "production",
-  // or 'process.env.NODE_ENV': "development",
 });
