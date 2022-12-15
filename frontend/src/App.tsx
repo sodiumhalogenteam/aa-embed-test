823ee9610a516a71ea50e0895a9212a2f3aac261
Nikki Dodge <nikki@sodiumhalogen.com>
2022-12-15 13:29:35 -0700

    📦  FEAT: [App] add version number

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import "./App.css";
-
-function App() {
-  return <div className="App">Inner app here</div>;
+import { VERSION } from "./VERSION";
+interface AppProps {
+  user: { userId: string };
+}
+function App(props: AppProps) {
+  return (
+    <div className="App">
+      Inner app here {props.user.userId}v{VERSION}
+    </div>
+  );
 }
 
 export default App;
